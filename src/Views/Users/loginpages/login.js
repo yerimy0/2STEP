5bc6573eb7f33ca47d933510c239f6bff4986856
임예림 <dpflachlrh@gmail.com>
2024-02-23 00:20:05 +0900

    Feat:로그인페이지 이메일 유효성 검사 기능 추가

--- src/Views/Users/loginpages/login.js
+++ src/Views/Users/loginpages/login.js
@@ -1,3 +1,9 @@
+function isValidEmail(email) {
+    // 이메일 정규식 패턴
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}
+
 function login() {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
@@ -5,7 +11,12 @@ function login() {
     // 이메일 또는 비밀번호가 입력되지 않은 경우
     if (email === '' || password === '') {
         alert('이메일 또는 비밀번호를 입력해 주세요');
-    } else {
+    } 
+    // 유효하지 않은 이메일 형식인 경우
+    else if (!isValidEmail(email)) {
+        alert('유효하지 않은 이메일 형식입니다');
+    }
+    else {
         // 예시로 고정된 계정 정보를 사용하여 로그인
         if (email === 'example@example.com' && password === 'password') {
             window.location.href = 'success_page.html'; // 메인 페이지로 url 변경
