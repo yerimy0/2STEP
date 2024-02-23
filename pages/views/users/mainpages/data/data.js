bfc410ca36533a5fa3887c8aed7c73c8eb094afe
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2024-02-23 21:58:01 +0900

    Rename: views 디렉토리 및 README 문서 경로 수정

---
+++ pages/views/users/mainpages/data/data.js
@@ -0,0 +1,113 @@
+export const PRODUCT_DATA = [
+  {
+    image:
+      "https://qi-o.qoo10cdn.com/goods_image_big/9/8/2/4/11602479824_l.jpg",
+    name: "신발 이름1",
+    category: "MEN",
+    currentPrice: 200000,
+    discount: 28,
+  },
+  {
+    image:
+      "https://qi-o.qoo10cdn.com/goods_image_big/9/8/2/4/11602479824_l.jpg",
+    name: "신발 이름2",
+    category: "MEN",
+    currentPrice: 100000,
+    discount: 30,
+  },
+  {
+    image:
+      "https://qi-o.qoo10cdn.com/goods_image_big/9/8/2/4/11602479824_l.jpg",
+    name: "신발 이름3",
+    category: "WOMEN",
+    currentPrice: 30,
+    discount: 10,
+  },
+  {
+    image:
+      "https://qi-o.qoo10cdn.com/goods_image_big/9/8/2/4/11602479824_l.jpg",
+    name: "신발 이름4",
+    category: "WOMEN",
+    currentPrice: 100000000000000,
+    discount: 50,
+  },
+  {
+    image:
+      "https://qi-o.qoo10cdn.com/goods_image_big/9/8/2/4/11602479824_l.jpg",
+    name: "신발 이름5",
+    category: "LIFE",
+    currentPrice: 200000,
+    discount: 100,
+  },
+  {
+    image:
+      "https://qi-o.qoo10cdn.com/goods_image_big/9/8/2/4/11602479824_l.jpg",
+    name: "신발 이름6",
+    category: "ACTIVE",
+    currentPrice: 100000,
+    discount: 1,
+  },
+  {
+    image:
+      "https://qi-o.qoo10cdn.com/goods_image_big/9/8/2/4/11602479824_l.jpg",
+    name: "신발 이름7",
+    category: "MEN",
+    currentPrice: 30,
+    discount: 90,
+  },
+  {
+    image:
+      "https://qi-o.qoo10cdn.com/goods_image_big/9/8/2/4/11602479824_l.jpg",
+    name: "신발 이름8",
+    category: "ACTIVE",
+    currentPrice: 100000000000000,
+    discount: 70,
+  },
+  {
+    image:
+      "https://qi-o.qoo10cdn.com/goods_image_big/9/8/2/4/11602479824_l.jpg",
+    name: "신발 이름9",
+    category: "MEN",
+    currentPrice: 200000,
+    discount: 100,
+  },
+  {
+    image:
+      "https://qi-o.qoo10cdn.com/goods_image_big/9/8/2/4/11602479824_l.jpg",
+    name: "신발 이름10",
+    category: "MEN",
+    currentPrice: 100000,
+    discount: 100,
+  },
+  {
+    image:
+      "https://qi-o.qoo10cdn.com/goods_image_big/9/8/2/4/11602479824_l.jpg",
+    name: "신발 이름11",
+    category: "MEN",
+    currentPrice: 30,
+    discount: 100,
+  },
+  {
+    image:
+      "https://qi-o.qoo10cdn.com/goods_image_big/9/8/2/4/11602479824_l.jpg",
+    name: "신발 이름12",
+    category: "MEN",
+    currentPrice: 100000000000000,
+    discount: 100,
+  },
+];
+
+export const TILTE_TEXT = [
+  {
+    title: "지금 가장 인기있는 상품👍",
+    subTitle: "투스탭 특가템 최대 50%",
+  },
+  {
+    title: "2STEP의 최대 할인 상품",
+    subTitle: "투스탭 특가템 최대 100%",
+  },
+  {
+    title: "마지막 품절 상품",
+    subTitle: "공짜다!",
+  },
+];
