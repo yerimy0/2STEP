6f75840d931fa6161f7fed6026ab5a740a33311e
임예림 <dpflachlrh@gmail.com>
2024-02-26 13:59:36 +0900

    Feat:로그인페이지 리뷰 내용 수정

--- pages/views/users/loginpages/login.js
+++ pages/views/users/loginpages/login.js
@@ -1,8 +1,17 @@
-function isValidEmail(email) {
-    // 이메일 정규식 패턴
-    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailPattern.test(email);
-}
+document.querySelector(".login-btn").addEventListener("click", function(event) {
+    event.preventDefault();
+    login(); 
+});
+
+document.querySelector(".signup-btn").addEventListener("click", function(event) {
+    event.preventDefault();
+    signup(); 
+});
+
+document.querySelector(".adminlogin-link").addEventListener("click", function(event) {
+    event.preventDefault();
+    adminLogin(); 
+});
 
 function login() {
     let email = document.getElementById('email').value;
@@ -11,10 +20,12 @@ function login() {
     // 이메일 또는 비밀번호가 입력되지 않은 경우
     if (email === '' || password === '') {
         alert('이메일 또는 비밀번호를 입력해 주세요');
+        return;
     }     
     // 유효하지 않은 이메일 형식인 경우
     else if (!isValidEmail(email)) {
         alert('유효하지 않은 이메일 형식입니다');
+        return;
     } else {
         // 예시로 고정된 계정 정보를 사용하여 로그인
         if (email === 'example@example.com' && password === 'password') {
@@ -25,12 +36,15 @@ function login() {
     }
 }
 
-function signup() {
-    // 회원가입 페이지로 이동
-    window.location.href = '회원가입 페이지 URL';
+function isValidEmail(email) {
+    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
 }
 
-function adminlogin() {
-    // 관리자 로그인 페이지로 이동
-    window.location.href = '관리자 로그인 페이지 URL';
+function signup() {
+    window.location.href = 'URL';
+}
+
+function adminLogin() {
+    window.location.href = 'URL';
 }
