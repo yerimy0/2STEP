1039ea3f13eb1c878c455abbb95c3dc6f86af43c
임예림 <dpflachlrh@gmail.com>
2024-02-27 02:07:42 +0900

    Feat:주문내역페이지 구현

--- pages/views/users/mypage/orderdetails.js
+++ pages/views/users/mypage/orderdetails.js
@@ -6,27 +6,21 @@ document.addEventListener("DOMContentLoaded", function() {
 // 주문 내역 표시 함수
 function showOrderHistory() {
     document.querySelector(".main-content").innerHTML = `
-      <div class="page-title">주문내역</div>
-      <div class="separator"></div>
-      <div class="order-item">
+    <div class="page-title">주문내역</div>
+    <div class="separator"></div>
+    <div class="order-item">
+      <div class="image-group">
         <img src="product1.jpg" alt="Product 1">
-        <div class="order-item-details">
-          <h3>상품명: 상품 1</h3>
-          <p>가격: $20</p>
-          <p>배송상태: 배송중</p>
-          <button class="order-button">주문 취소</button>
-          <button class="order-button">주문 정보 수정</button>
-        </div>
       </div>
-      <div class="order-item">
-        <img src="product2.jpg" alt="Product 2">
-        <div class="order-item-details">
-          <h3>상품명: 상품 2</h3>
-          <p>가격: $30</p>
-          <p>배송상태: 배송완료</p>
-          <button class="order-button">주문 취소</button>
-          <button class="order-button">주문 정보 수정</button>
-        </div>
+      <div class="details-group">
+        <h3>NOCTA x 나이키 핫 스탭 에어 테라</h3>
+        <p>139,000원</p>
+        <p class="delivery-status">배송준비중</p>
       </div>
-    `;
+      <div class="button-group">
+        <button class="order-button">주문 취소</button>
+        <button class="order-button">주문 수정</button>
+      </div>
+    </div>
+  `;
 }
