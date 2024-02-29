ee653de589f78d7c4bdc234bb420b896a12a2ccf
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2024-02-29 17:45:09 +0900

    #2 feat: API 로그인 토큰 연결

--- pages/views/users/loginpages/login.js
+++ pages/views/users/loginpages/login.js
@@ -1,40 +1,43 @@
 // 로그인 처리 함수
 async function handleLogin() {
-  const email = document.querySelector('.email').value; // 이메일 입력값 가져오기
-  const password = document.querySelector('.password').value; // 비밀번호 입력값 가져오기
-  
-// 이메일 또는 비밀번호가 입력되지 않은 경우 
-  if (email === '' || password === '') {
-    alert('이메일 또는 비밀번호를 입력해 주세요');
+  const email = document.querySelector(".email").value; // 이메일 입력값 가져오기
+  const password = document.querySelector(".password").value; // 비밀번호 입력값 가져오기
+
+  // 이메일 또는 비밀번호가 입력되지 않은 경우
+  if (email === "" || password === "") {
+    alert("이메일 또는 비밀번호를 입력해 주세요");
     return;
   }
   if (!isValidEmail(email)) {
-    alert('유효하지 않은 이메일 형식입니다');
+    alert("유효하지 않은 이메일 형식입니다");
     return;
-}
- // API 수정 필요
+  }
+  // API 수정 필요
   try {
-      const data = { email, password };
-      const response = await fetch('http://kdt-sw-8-team02.elicecoding.com/api/v1/auth/sign-in', {
-          method: 'POST',
-          headers: {
-              'Content-Type' : 'application/json'
-          },
-          body: JSON.stringify({ ...data })
-      });
+    const data = { email, password };
+    const response = await fetch(
+      "http://kdt-sw-8-team02.elicecoding.com/api/v1/auth/sign-in",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ...data }),
+      }
+    );
 
-      if (response.ok) {
-          const data = await response.json();
-          const token = data.token; // 서버에서 받은 토큰
-          localStorage.setItem('token', token); // 토큰을 로컬 스토리지에 저장
-          window.location.href = 'success_page.html'; // 로그인 성공 시 페이지 이동
-      } else {
-          const errorMessage = await response.text();
-          alert(`로그인 실패: ${errorMessage}`);
-      } 
-  } catch(error) {
-      console.error('로그인 요청 실패:', error);
-      alert('로그인 요청에 실패했습니다. 다시 시도해주세요');
+    if (response.ok) {
+      const data = await response.json();
+      const token = data.data; // 서버에서 받은 토큰
+      localStorage.setItem("token", token); // 토큰을 로컬 스토리지에 저장
+      window.location.href =
+        "http://localhost:8080/views/users/mainpages/mainpage.html"; // 로그인 성공 시 페이지 이동
+    } else {
+      alert(`로그인 실패`);
+    }
+  } catch (error) {
+    console.error("로그인 요청 실패:", error);
+    alert("로그인 요청에 실패했습니다. 다시 시도해주세요");
   }
 }
 
@@ -45,15 +48,15 @@ function isValidEmail(email) {
 }
 
 // 클릭 이벤트 리스너
-document.addEventListener("click", function(event) {
+document.addEventListener("click", function (event) {
   event.preventDefault();
   const target = event.target;
-  
+
   if (target.classList.contains("login-btn")) {
-      handleLogin(); // 로그인 함수 호출
+    handleLogin(); // 로그인 함수 호출
   } else if (target.classList.contains("signup-btn")) {
-      window.location.href = "URL"; // 회원가입 페이지로 이동
+    window.location.href = "URL"; // 회원가입 페이지로 이동
   } else if (target.classList.contains("adminlogin-link")) {
-      window.location.href = "URL"; // 관리자 로그인 페이지로 이동
+    window.location.href = "URL"; // 관리자 로그인 페이지로 이동
   }
 });
