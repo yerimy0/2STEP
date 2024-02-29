da161e59bbf6ca66d26ee9724776bf5e86de36f3
Gyeong jin <ghrutntn@gmail.com>
2024-02-29 21:49:56 +0900

    feat: 결제 완료 구현

--- pages/views/users/mainpages/js/imageslide.js
+++ pages/views/users/mainpages/js/imageslide.js
@@ -2,18 +2,18 @@ let topBannerImageIdx = 0;
 let eventBannerImagesIdx = 0;
 //예시
 const images = [
-  "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20160909_245%2Fvtotv93_1473393461771rMdCp_JPEG%2FNIKE-ZOOM-PEGASUS-92-844652_001_C_PREM.jpg&type=sc960_832",
-  "https://search.pstatic.net/common/?src=http%3A%2F%2Fpost.phinf.naver.net%2FMjAyMDEyMjZfMjI3%2FMDAxNjA4OTA5OTk2MjEw.jL2RkhIldE52qSQXF5XHya_Sv3CMPeZLS1ymS2fHiDEg.uzstdGDkhaBEInSX_3HmvPxjmjPRCxOBXsfMQvJlzRwg.JPEG%2FIAXHFt3DRwthCar63bqLg-XZ87_M.jpg&type=sc960_832",
-  "https://qi-o.qoo10cdn.com/goods_image_big/9/8/2/4/11602479824_l.jpg",
+  "https://i.pinimg.com/originals/0d/be/18/0dbe18421a5230e8129a8f605deb61a4.jpg",
+  "https://i.pinimg.com/originals/d1/26/8f/d1268f371c94a120b18a580e364f9d9b.png",
+  "https://ecimg.cafe24img.com/pg90b05313110010/brooksrunning/web/upload/appfiles/ZaReJam3QiELznoZeGGkMG/b879ca327dc52ccb1cbdcbbfd0d8ee13.jpg",
 ];
 
 const eventImages = [
-  "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDA0MTBfMjUw%2FMDAxNTg2NDc3MTI2ODYx.pubtznHS1ZgxyTZgbf_sZcskNzYkwQJaby7-0pLuOCIg.4la6BlPeBchplo88yWwxDfFQfCjevIywSsPuwJ3ZDjsg.PNG.kybunswitzerland%2F%25BB%25E7%25BF%25AC%25C0%25CC%25BA%25A5%25C6%25AE_%25B4%25E7%25C3%25B7%25C8%25C4%25B1%25E2.png&type=sc960_832",
-  "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDA0MTdfMTYy%2FMDAxNTg3MDg3ODY2NDY1.S9_IgaaGwyXrHpdoPri-unBidnT4hTTYi3GRn_6tf-Ug.EMaknTYhpBASeVh-Vlnc_k7jbi5oR0-TKoCDMi02b7gg.PNG.kybunswitzerland%2F1%25BF%25F9_%25BD%25E6%25B3%25D7%25C0%25CF.png&type=sc960_832",
+  "https://shoerio7882.cafe24.com/shoerio/main/mainEventBnn_M4.jpg",
+  "https://shoerio7882.cafe24.com/shoerio/main/mainEventBnn_M1.jpg",
 ];
 
 const weeklyImage = [
-  "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAxOTA0MTZfMjkw%2FMDAxNTU1Mzk2NDEyODc4.kBmZBvoFi1vUVf07vl-rqIKqAAWoAIRTDM-7qxPjdAwg.2tGrfZzUFXbthRw4FacPnqGvsqvaB_R-ZlEjVztdodog.JPEG.wash5287%2F%25C2%25F7%25B7%25AE%25B9%25F8%25C8%25A3%25C0%25CC%25BA%25A5%25C6%25AE%25C6%25E4%25C0%25CC%25C1%25F6__20190416.jpg&type=sc960_832",
+  "https://shunishuu.cafe24.com/morenvyimg/1+1J.jpg",
 ];
 
 const weeklyBestBannerImage = document.getElementById("weeklyBestBannerImage");
